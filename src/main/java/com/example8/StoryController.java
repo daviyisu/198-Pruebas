be96e6401582bfed818386626ae566bec2754ad8
davidjesusruiz <davidjesus.ruiz@nucleoo.com>
2023-05-11 10:53:21 +0200

    Initial commit

--- src/main/java/com/example8/StoryController.java
+++ src/main/java/com/example8/StoryController.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
